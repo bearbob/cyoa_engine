9cf9742ed220baa54873140853e1e1eb9004b41b
Björn Schmidt <bearbob@users.noreply.github.com>
2023-03-05 13:33:11 +0100

    Update NavigationOption.kt

--- src/main/kotlin/net/tripletwenty/coya/core/entities/NavigationOption.kt
+++ src/main/kotlin/net/tripletwenty/coya/core/entities/NavigationOption.kt
@@ -24,28 +24,48 @@ class NavigationOption(
     }
 
     fun isAvailable(state: State): Boolean {
-        getConditions().forEach {
-            val varValue = state.items
-            TODO()
+        getConditions().forEach { c ->
+            if (!isConditionSatisfied(c, state)) {
+                return false
+            }
         }
         return true
     }
 
+    private fun isConditionSatisfied(condition: Condition, state: State): Boolean {
+        return if ( condition.type == Condition.Type.EVENT) {
+            state.events?.any { it.eventLabel == condition.variable } ?: false
+        } else {
+            val itemValue = state.items?.firstOrNull { it.itemLabel == condition.variable }?.amount ?: 0
+            when (condition.comparator!!) {
+                Condition.Comparator.EQUAL -> itemValue == condition.value!!
+                Condition.Comparator.GEQ -> itemValue >= condition.value!!
+                Condition.Comparator.GREATER -> itemValue > condition.value!!
+                Condition.Comparator.LESS -> itemValue < condition.value!!
+                Condition.Comparator.LEQ -> itemValue <= condition.value!!
+                Condition.Comparator.NEQ -> itemValue != condition.value!!
+            }
+        }
+    }
 }
 
 data class Condition(
     val variable: String,
     val value: Int?,
-    val comparator: Comparator
-)
+    val comparator: Comparator?,
+    val type: Type,
+) {
+    enum class Type {
+        EVENT,
+        ITEM
+    }
 
-enum class Comparator {
-    HAPPENED,
-    NOT_HAPPENED,
-    EQUAL,
-    LESS,
-    GREATER,
-    LEQ,
-    NEQ,
-    GEQ;
+    enum class Comparator {
+        EQUAL,
+        LESS,
+        GREATER,
+        LEQ,
+        NEQ,
+        GEQ;
+    }
 }
